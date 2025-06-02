26b64f44d4c33a4bf9eb44838a7c6eef52bdcb84
Rishav-R03 <rishav042023@gmail.com>
2025-06-03 00:33:44 +0530

    database modification

--- backend/databaseConn.js
+++ backend/databaseConn.js
@@ -6,6 +6,7 @@ const conn = async () => {
     await mongoose.connect(process.env.MONGO_URI)
     .then(()=>console.log("Database connected!"))
     .catch((err)=>{console.log("Error connecting database",err.message)})
+    process.exit(1)
 }
 
 export default conn;
