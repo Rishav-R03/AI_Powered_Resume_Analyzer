b7ad91707cb011edbe69a9cfab5afb9fa9fb7cf1
Rishav-R03 <rishav042023@gmail.com>
2025-06-03 00:34:40 +0530

    database connection

--- backend/databaseConn.js
+++ backend/databaseConn.js
@@ -4,9 +4,9 @@ dotenv.config()
 
 const conn = async () => {
     await mongoose.connect(process.env.MONGO_URI)
-    .then(()=>console.log("Database connected!"))
-    .catch((err)=>{console.log("Error connecting database",err.message)})
-    process.exit(1)
+        .then(() => console.log("Database connected!"))
+        .catch((err) => { console.log("Error connecting database", err.message), process.exit(1) })
+
 }
 
 export default conn;
