bae61519662f1fc9dc30941c88c979787b9676fa
Rishav-R03 <rishav042023@gmail.com>
2025-06-03 00:32:44 +0530

    database connection

---
+++ backend/databaseConn.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv';
+dotenv.config()
+
+const conn = async () => {
+    await mongoose.connect(process.env.MONGO_URI)
+    .then(()=>console.log("Database connected!"))
+    .catch((err)=>{console.log("Error connecting database",err.message)})
+}
+
+export default conn;
