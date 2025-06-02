48de55ea7a93246074f60299d64c3fc2d7832135
Rishav-R03 <rishav042023@gmail.com>
2025-06-03 00:49:01 +0530

    database connection

--- backend/databaseConn.js
+++ backend/databaseConn.js
@@ -3,10 +3,20 @@ import dotenv from 'dotenv';
 dotenv.config()
 
 const conn = async () => {
-    await mongoose.connect(process.env.MONGO_URI)
-        .then(() => console.log("Database connected!"))
-        .catch((err) => { console.log("Error connecting database", err.message), process.exit(1) })
+    if (!process.env.MONGO_URI) {
+        console.error('CRITICAL ERROR: MONGODB_URI environment variable not set.');
+        // Exit the process if the crucial environment variable is missing
+        process.exit(1);
+    }
 
-}
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error connecting to MongoDB: ${error.message}`);
+        // Exit process with failure
+        process.exit(1);
+    }
+};
 
 export default conn;
